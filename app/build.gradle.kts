25810abaeeac0198fd9d94e95e715f92d3874a98
aurexvelion <leinsamppa@gmail.com>
2026-09-06 23:05:32 +0300

    Version LUNAR III

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins { id("com.android.application") }
 android {
     namespace = "com.lunar.genshin"
     compileSdk = 36
-    defaultConfig { applicationId = "com.lunar.genshin"; minSdk = 31; targetSdk = 36; versionCode = 6; versionName = "0.6-s25" }
+    defaultConfig { applicationId = "com.lunar.genshin"; minSdk = 31; targetSdk = 36; versionCode = 7; versionName = "1.0-lunar3" }
     buildTypes { release { isMinifyEnabled = false } }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
 }
